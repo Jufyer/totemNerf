47281686942430caee1a2a5e660512b4823f47c7
Jufyer <julianders@gmx.net>
2024-12-21 19:36:26 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
+plugins {
+  `java-library`
+  id("io.papermc.paperweight.userdev") version "1.7.3"
+  id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
+}
+
+group = "org.jufyer.plugin"
+version = "1.0.0-SNAPSHOT"
+description = "This Plugin applies a Totem  Nerf"
+
+java {
+  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
+  toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+dependencies {
+  paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+}
+
+tasks {
+  compileJava {
+    options.release = 21
+  }
+  javadoc {
+    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+  }
+}
+
+// Configure plugin.yml generation
+// - name, version, and description are inherited from the Gradle project.
+bukkitPluginYaml {
+  main = "org.jufyer.plugin.totemNerf.Main"
+  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+  authors.add("Jufyer")
+  apiVersion = "1.21.4"
+  commands {
+
+  }
+}
---
+++ src/main/java/org/jufyer/plugin/totemNerf/Main.java
@@ -0,0 +1,22 @@
+package org.jufyer.plugin.totemNerf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin{
+  private static Main instance;
+
+  public static Main getInstance() {
+    return instance;
+  }
+
+  @Override
+  public void onEnable() {
+    instance = this;
+
+  }
+
+  @Override
+  public void onDisable(){
+
+  }
+}
